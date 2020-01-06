6ff6482e1b50af2ad630846b7f9a82e6f92d5b18
Thomas Charon <charont@miashs-b1-13.u-ga.fr>
2020-01-06 13:05:12 +0100

    initialisation grille

--- game.js
+++ game.js
@@ -6,6 +6,19 @@
     3: reine noire
     4: reine blanche
 */
+var plateauBasic = [
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 2, 0, 2, 0, 2, 0, 2, 0, 2],
+    [2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
+    [0, 2, 0, 2, 0, 2, 0, 2, 0, 2],
+    [2, 0, 2, 0, 2, 0, 2, 0, 2, 0]
+];
+
 var plateau = [
     [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
     [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
@@ -19,117 +32,120 @@ var plateau = [
     [2, 0, 2, 0, 2, 0, 2, 0, 2, 0]
 ];
 
-var noeud = document.getElementById("grille");
-var filsNoeud = noeud.childNodes;
+var noeud;
+var filsNoeud;
+
+var initialiserGrille = function () {
+    plateauBasic.forEach(function (item, index, array) {
+        plateau[index] = item.slice(0);
+    })
+    grille = document.getElementById("container");
+    while (grille.hasChildNodes()) {
+        grille.removeChild(grille.firstChild);
+    }
+    grille.appendChild(grid(50, ["yellow", "brown"]));
+    joueurs = ["joueur1"];
+
+    noeud = document.getElementById("grille");
+    filsNoeud = noeud.childNodes;
+}
 
 /**
  * Cette fonction est appelé dans tous les cas une fois que la fonction caseSelect terminé
  * Elle permet de gérer la régle de prise obligatoire de la pièce adverse.
  * @param {string=} joueur 
  */
-var pionABouger = function(joueur){
+var pionABouger = function (joueur) {
     // un tableau pour stocker les pions qu'on a droit de bouger(cas prise obligatoire) 
-    var tabPionABouger=[];
-    var count =0;
+    var tabPionABouger = [];
+    var count = 0;
     //joueur pions noir
-    if(joueur=="joueur1"){
-      //parcourir le tableau plateau pour voir s'il y a des cas de prise obligatoire
-      for (var i = 0; i < 10; i++) {
-         for (var j = 0; j < 10; j++) {
-             //si on trouve un ou plusieur cas de prise obligatoire
-            if((plateau[i][j]==1 && plateau[i+1][j+1]==2 && plateau[i+2][j+2]==0)|| (plateau[i][j]==1 && plateau[i+1][j-1]==2 && plateau[i+2][j-2]==0)){
-                var id = "lig"+i+" col"+j;
-                var lig = i;
-                var col = j; 
-                //stocker les id des pions(min = 1 pion , max = 2 pion) qu'il faut bouger(prise obligatoire)
-                tabPionABouger[count]=id;
-                count++;
-               
-                //si un seul cas de prise obligatoire (c.a.d tabPionABouger.length==1)
-                if(tabPionABouger.length==1){
-                    /*parcourir tous les noeud fils de l'élément svgGrid pour:
-                    -supprimer l'évenemnt onclick sur tous les pions sauf celui/ceux qu'il faut bouger (prise obligatoire)
-                    -supprimer l'évenemnt onclick sur toutes les cases sauf celle/celles vers lesquelles les pions seront déplacer(prise obligatoire)*/
-                    for(var h=0; h<filsNoeud.length;h++){
-                    if(filsNoeud[h].getAttribute("onclick")=="selectPion(this.id)" &&  filsNoeud[h].getAttribute("fill") == "black" && filsNoeud[h].getAttribute("id")!=tabPionABouger[0]){
-                        filsNoeud[h].removeAttribute("onclick");
-                        console.log("joueur1 if");
-                    }
-                    if(filsNoeud[h].getAttribute("onclick")=="caseSelct(this.id)" && ((filsNoeud[h].getAttribute("id")!="Lig"+(i+2)+" Col"+(j+2) && plateau[i+1][j+1]==2) ||
-                    (filsNoeud[h].getAttribute("id")!="Lig"+(i+2)+" Col"+(j-2) && plateau[i+1][j-1]==2))){
-                        filsNoeud[h].removeAttribute("onclick");
-                    }
-                    }
-                //si deux cas de prise obligatoire (c.a.d tabPionABouger.length==2)    
-                }else if(tabPionABouger.length==2){
-                    for(var h=0; h<filsNoeud.length;h++){
-                    if(filsNoeud[h].getAttribute("onclick")=="selectPion(this.id)" &&  filsNoeud[h].getAttribute("fill") == "black"){
-                    filsNoeud[h].removeAttribute("onclick");
-                    }
-                    if(filsNoeud[h].getAttribute("onclick")=="caseSelct(this.id)"){
-                        filsNoeud[h].removeAttribute("onclick");
-                    }
-                    
-                    }
-                    
-                    document.getElementById(tabPionABouger[0]).setAttribute("onclick","selectPion(this.id)");
-                    document.getElementById(tabPionABouger[1]).setAttribute("onclick","selectPion(this.id)");
-                    document.getElementById("Lig"+(parseInt(tabPionABouger[0].substring(3,4))+2)+" Col"+parseInt(tabPionABouger[0].substring(8))).setAttribute("onclick","caseSelct(this.id)");
-                    document.getElementById("Lig"+(parseInt(tabPionABouger[1].substring(3,4))+2)+" Col"+parseInt(tabPionABouger[1].substring(8))).setAttribute("onclick","caseSelct(this.id)");
-                } 
-            }
+    if (joueur == "joueur1") {
+        //parcourir le tableau plateau pour voir s'il y a des cas de prise obligatoire
+        for (var i = 0; i < 10; i++) {
+            for (var j = 0; j < 10; j++) {
+                //si on trouve un ou plusieur cas de prise obligatoire
+                if ((plateau[i][j] == 1 && plateau[i + 1][j + 1] == 2 && plateau[i + 2][j + 2] == 0) || (plateau[i][j] == 1 && plateau[i + 1][j - 1] == 2 && plateau[i + 2][j - 2] == 0)) {
+                    var id = "lig" + i + " col" + j;
+                    //stocker les id des pions(min = 1 pion , max = 2 pion) qu'il faut bouger(prise obligatoire)
+                    tabPionABouger[count] = id;
+                    count++;
 
-         }  
-      }
-     
-    }
-
-   if(joueur=="joueur2"){
-      for (var i = 0; i < 10; i++) {
-         for (var j = 0; j < 10; j++) {
-            if((plateau[i][j]==2 && plateau[i-1][j+1]==1 && plateau[i-2][j+2]==0)|| (plateau[i][j]==2 && plateau[i-1][j-1]==1 && plateau[i-2][j-2]==0)){
-                var id = "lig"+i+" col"+j;
-                var lig = i;
-                var col = j; 
-                tabPionABouger[count]=id;  
-                count++;
-               
-            
-                
-                if(tabPionABouger.length==1){
-                    for(var h=0; h<filsNoeud.length;h++){
-                    if(filsNoeud[h].getAttribute("onclick")=="selectPion(this.id)" &&  filsNoeud[h].getAttribute("fill") == "white" && filsNoeud[h].getAttribute("id")!=tabPionABouger[0]){
-                        filsNoeud[h].removeAttribute("onclick");
-                        console.log("joueur1 if");
-                    }
-                    if(filsNoeud[h].getAttribute("onclick")=="caseSelct(this.id)" && ((filsNoeud[h].getAttribute("id")!="Lig"+(i-2)+" Col"+(j+2) && plateau[i-1][j+1]==1) ||
-                    (filsNoeud[h].getAttribute("id")!="Lig"+(i-2)+" Col"+(j-2) && plateau[i-1][j-1]==1))){
-                        filsNoeud[h].removeAttribute("onclick");
-                    }
-                    }
-                }else{
-                    for(var h=0; h<filsNoeud.length;h++){
-                        if(filsNoeud[h].getAttribute("onclick")=="selectPion(this.id)" &&  filsNoeud[h].getAttribute("fill") == "white"){
-                        filsNoeud[h].removeAttribute("onclick");
+                    //si un seul cas de prise obligatoire (c.a.d tabPionABouger.length==1)
+                    if (tabPionABouger.length == 1) {
+                        /*parcourir tous les noeud fils de l'élément svgGrid pour:
+                        -supprimer l'évenemnt onclick sur tous les pions sauf celui/ceux qu'il faut bouger (prise obligatoire)
+                        -supprimer l'évenemnt onclick sur toutes les cases sauf celle/celles vers lesquelles les pions seront déplacer(prise obligatoire)*/
+                        for (var h = 0; h < filsNoeud.length; h++) {
+                            if (filsNoeud[h].getAttribute("onclick") == "selectPion(this.id)" && filsNoeud[h].getAttribute("fill") == "black" && filsNoeud[h].getAttribute("id") != tabPionABouger[0]) {
+                                filsNoeud[h].removeAttribute("onclick");
+                                console.log("joueur1 if");
+                            }
+                            if (filsNoeud[h].getAttribute("onclick") == "caseSelct(this.id)" && ((filsNoeud[h].getAttribute("id") != "Lig" + (i + 2) + " Col" + (j + 2) && plateau[i + 1][j + 1] == 2) ||
+                                (filsNoeud[h].getAttribute("id") != "Lig" + (i + 2) + " Col" + (j - 2) && plateau[i + 1][j - 1] == 2))) {
+                                filsNoeud[h].removeAttribute("onclick");
+                            }
                         }
-                        if(filsNoeud[h].getAttribute("onclick")=="caseSelct(this.id)"){
-                        filsNoeud[h].removeAttribute("onclick");
+                        //si deux cas de prise obligatoire (c.a.d tabPionABouger.length==2)    
+                    } else if (tabPionABouger.length == 2) {
+                        for (var h = 0; h < filsNoeud.length; h++) {
+                            if (filsNoeud[h].getAttribute("onclick") == "selectPion(this.id)" && filsNoeud[h].getAttribute("fill") == "black") {
+                                filsNoeud[h].removeAttribute("onclick");
+                            }
+                            if (filsNoeud[h].getAttribute("onclick") == "caseSelct(this.id)") {
+                                filsNoeud[h].removeAttribute("onclick");
+                            }
+                        }
+
+                        document.getElementById(tabPionABouger[0]).setAttribute("onclick", "selectPion(this.id)");
+                        document.getElementById(tabPionABouger[1]).setAttribute("onclick", "selectPion(this.id)");
+                        document.getElementById("Lig" + (parseInt(tabPionABouger[0].substring(3, 4)) + 2) + " Col" + parseInt(tabPionABouger[0].substring(8))).setAttribute("onclick", "caseSelct(this.id)");
+                        document.getElementById("Lig" + (parseInt(tabPionABouger[1].substring(3, 4)) + 2) + " Col" + parseInt(tabPionABouger[1].substring(8))).setAttribute("onclick", "caseSelct(this.id)");
                     }
-                    } 
-                    document.getElementById(tabPionABouger[0]).setAttribute("onclick","selectPion(this.id)");
-                    document.getElementById(tabPionABouger[1]).setAttribute("onclick","selectPion(this.id)");
-                    
-                    document.getElementById("Lig"+(parseInt(tabPionABouger[0].substring(3,4))-2)+" Col"+parseInt(tabPionABouger[0].substring(8))).setAttribute("onclick","caseSelct(this.id)");
-                    document.getElementById("Lig"+(parseInt(tabPionABouger[1].substring(3,4))-2)+" Col"+parseInt(tabPionABouger[1].substring(8))).setAttribute("onclick","caseSelct(this.id)");
-                } 
+                }
             }
-        
-         }
-      }
-     
+        }
     }
-     
-  }
+
+    if (joueur == "joueur2") {
+        for (var i = 0; i < 10; i++) {
+            for (var j = 0; j < 10; j++) {
+                if ((plateau[i][j] == 2 && plateau[i - 1][j + 1] == 1 && plateau[i - 2][j + 2] == 0) || (plateau[i][j] == 2 && plateau[i - 1][j - 1] == 1 && plateau[i - 2][j - 2] == 0)) {
+                    var id = "lig" + i + " col" + j;
+                    tabPionABouger[count] = id;
+                    count++;
+
+                    if (tabPionABouger.length == 1) {
+                        for (var h = 0; h < filsNoeud.length; h++) {
+                            if (filsNoeud[h].getAttribute("onclick") == "selectPion(this.id)" && filsNoeud[h].getAttribute("fill") == "white" && filsNoeud[h].getAttribute("id") != tabPionABouger[0]) {
+                                filsNoeud[h].removeAttribute("onclick");
+                                console.log("joueur1 if");
+                            }
+                            if (filsNoeud[h].getAttribute("onclick") == "caseSelct(this.id)" && ((filsNoeud[h].getAttribute("id") != "Lig" + (i - 2) + " Col" + (j + 2) && plateau[i - 1][j + 1] == 1) ||
+                                (filsNoeud[h].getAttribute("id") != "Lig" + (i - 2) + " Col" + (j - 2) && plateau[i - 1][j - 1] == 1))) {
+                                filsNoeud[h].removeAttribute("onclick");
+                            }
+                        }
+                    } else {
+                        for (var h = 0; h < filsNoeud.length; h++) {
+                            if (filsNoeud[h].getAttribute("onclick") == "selectPion(this.id)" && filsNoeud[h].getAttribute("fill") == "white") {
+                                filsNoeud[h].removeAttribute("onclick");
+                            }
+                            if (filsNoeud[h].getAttribute("onclick") == "caseSelct(this.id)") {
+                                filsNoeud[h].removeAttribute("onclick");
+                            }
+                        }
+                        document.getElementById(tabPionABouger[0]).setAttribute("onclick", "selectPion(this.id)");
+                        document.getElementById(tabPionABouger[1]).setAttribute("onclick", "selectPion(this.id)");
+
+                        document.getElementById("Lig" + (parseInt(tabPionABouger[0].substring(3, 4)) - 2) + " Col" + parseInt(tabPionABouger[0].substring(8))).setAttribute("onclick", "caseSelct(this.id)");
+                        document.getElementById("Lig" + (parseInt(tabPionABouger[1].substring(3, 4)) - 2) + " Col" + parseInt(tabPionABouger[1].substring(8))).setAttribute("onclick", "caseSelct(this.id)");
+                    }
+                }
+            }
+        }
+    }
+}
 
 
 /**
@@ -137,43 +153,39 @@ var pionABouger = function(joueur){
  * --Cette fonction remet l'evenemnt "onclick" sur tous les pions et toutes les case après les avoir suprrimer dans la fonction PionABouger(prise obligatoire)
  * @param {string=} couleurPion 
  */
-var miseAjour = function(couleurPion){
+var miseAjour = function (couleurPion) {
     var noeud = document.getElementById("grille");
     var filsNoeud = noeud.childNodes;
-   
-      if(couleurPion=="black"){
-          /*parcourir tous les fils du noeud svgGrid pour ajouter les evenemnts "onclick" aux pions et cases*/
-        for(var g =0;g<filsNoeud.length;g++){
-            if(filsNoeud[g].getAttribute("fill")=="black"){
-            filsNoeud[g].setAttribute("onclick", "selectPion(this.id)");
-            
-            }
-        
-            if(filsNoeud[g].getAttribute("fill")=="brown"){
-                filsNoeud[g].setAttribute("onclick", "caseSelct(this.id)");
-                
-            }
-        
-        }
-      }
-      else if (couleurPion=="white"){
-          /*parcourir tous les fils du noeud svgGrid pour ajouter les evenemnts "onclick" aux pions et cases*/
-            for(var g =0;g<filsNoeud.length;g++){
-            if(filsNoeud[g].getAttribute("fill")=="white"){
-            filsNoeud[g].setAttribute("onclick", "selectPion(this.id)");
-            
-            }
-            
-        
-            if(filsNoeud[g].getAttribute("fill")=="brown"){
-                filsNoeud[g].setAttribute("onclick", "caseSelct(this.id)");
-                
-            }
-        
-        }
-      }
 
-  }
+    if (couleurPion == "black") {
+        /*parcourir tous les fils du noeud svgGrid pour ajouter les evenemnts "onclick" aux pions et cases*/
+        for (var g = 0; g < filsNoeud.length; g++) {
+            if (filsNoeud[g].getAttribute("fill") == "black") {
+                filsNoeud[g].setAttribute("onclick", "selectPion(this.id)");
+
+            }
+
+            if (filsNoeud[g].getAttribute("fill") == "brown") {
+                filsNoeud[g].setAttribute("onclick", "caseSelct(this.id)");
+
+            }
+        }
+    }
+    else if (couleurPion == "white") {
+        /*parcourir tous les fils du noeud svgGrid pour ajouter les evenemnts "onclick" aux pions et cases*/
+        for (var g = 0; g < filsNoeud.length; g++) {
+            if (filsNoeud[g].getAttribute("fill") == "white") {
+                filsNoeud[g].setAttribute("onclick", "selectPion(this.id)");
+
+            }
+
+            if (filsNoeud[g].getAttribute("fill") == "brown") {
+                filsNoeud[g].setAttribute("onclick", "caseSelct(this.id)");
+
+            }
+        }
+    }
+}
 
 //tableau pour gérer les tour de jeu, 
 var joueurs = ["joueur1"];
@@ -183,7 +195,7 @@ var elemClignote;
 
 /*varaible pour stocker le pion qui a été bouger avec les nouvelles cordonnées : 
 cette variable nous permet de voir si le joueur qui vient de jouer peut rejouer 
-encore( rafle par un pion) ou pas*/ 
+encore( rafle par un pion) ou pas*/
 var pionbouger;
 
 /**
@@ -209,7 +221,7 @@ var gereTourJeu = function (couleurPion) {
             selectPion(id);
             return "joueur1";
         } else
-        //sinon passer la main au joueur 2
+            //sinon passer la main au joueur 2
             return "joueur2";
     }
     //joueur 2
@@ -257,8 +269,8 @@ var selectPion = function (idPion) {
  */
 var caseSelct = function (idCell) {
     //case séléction (ligne, colonne)
-    var ligne = parseInt(idCell.substring(3,4));
-    var col = parseInt(idCell.substring(8,9));
+    var ligne = parseInt(idCell.substring(3, 4));
+    var col = parseInt(idCell.substring(8, 9));
 
     //cordonnées de la case
     var cell = document.getElementById(idCell);
@@ -273,13 +285,13 @@ var caseSelct = function (idCell) {
 
     //vérifier si y'a un pion sur la case
     var etatCase = plateau[ligne][col];
-    console.log("etat de la case : ",etatCase);
-    
-    
+    console.log("etat de la case : ", etatCase);
+
+
     /*-------------les cas de déplacement des pions --------------*/
 
     /*-------------déplacement sans gagner de piont --------------*/
-   
+
     //récupérer l'élemnt svgGrid
     var svg = document.getElementById("grille");
 
@@ -329,7 +341,7 @@ var caseSelct = function (idCell) {
                 //ajouter le pion au svgGrid
                 svg.appendChild(pion);
                 //supprimer le pion de son ancienne position 
-                svg.removeChild(elemClignote); 
+                svg.removeChild(elemClignote);
                 //identifier le pion qui a été déplacé pour le supprimer de son ancienne position
                 var idPionAdv = "lig" + (yCell - 50) / 50 + " col" + (xCell + 50) / 50;
                 var elementAdver = document.getElementById(idPionAdv);
@@ -349,9 +361,9 @@ var caseSelct = function (idCell) {
                 svg.removeChild(elementAdver);
                 plateau[(yCell - 50) / 50][(xCell - 50) / 50] = 0;
             }
-             //mettre à jour le plateau
-             plateau[ligne][col] = 1;
-             plateau[yPion / 50][xPion / 50] = 0;
+            //mettre à jour le plateau
+            plateau[ligne][col] = 1;
+            plateau[yPion / 50][xPion / 50] = 0;
 
             joueurs[0] = gereTourJeu("black");
             pionABouger(joueurs[0]);
@@ -385,9 +397,9 @@ var caseSelct = function (idCell) {
                 svg.removeChild(elementAdver);
                 plateau[(yCell + 50) / 50][(xCell - 50) / 50] = 0;
             }
-             //mettre à jour le plateau
-             plateau[ligne][col] = 2;
-             plateau[yPion / 50][xPion / 50] = 0;
+            //mettre à jour le plateau
+            plateau[ligne][col] = 2;
+            plateau[yPion / 50][xPion / 50] = 0;
 
             joueurs[0] = gereTourJeu("white");
             pionABouger(joueurs[0]);
